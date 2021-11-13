87ced27e91c42e74ae94b16efddacabd9cffe83c
COCOA-PC\Cocoa <mtj2010@naver.com>
2021-11-13 13:08:45 +0900

    신직업 개발 취소

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/abilities/gunner/GrenadeLauncher.java
+++
@@ -1,6 +0,0 @@
-package com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.gunner;
-
-import com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.ArmorAbility;
-
-public class GrenadeLauncher  extends ArmorAbility {
-}
