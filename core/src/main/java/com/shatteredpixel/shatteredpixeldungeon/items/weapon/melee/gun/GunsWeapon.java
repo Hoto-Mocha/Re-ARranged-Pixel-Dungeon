4fee94dab7ec49e435f682219e2a73d9664cb98d
COCOA-PC\Cocoa <mtj2010@naver.com>
2021-11-13 13:08:26 +0900

    총기 경로 변경

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/gun/GunsWeapon.java
+++
@@ -1,6 +0,0 @@
-package com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.gun;
-
-import com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.MeleeWeapon;
-
-public class GunsWeapon extends MeleeWeapon {
-}
