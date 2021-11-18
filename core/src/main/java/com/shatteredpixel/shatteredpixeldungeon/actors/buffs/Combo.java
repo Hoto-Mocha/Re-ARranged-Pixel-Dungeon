a4b6b52847b4697437db09d02c782cffbe87ae9e
COCOA-PC\Cocoa <mtj2010@naver.com>
2021-11-19 00:38:10 +0900

    변경 없음

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/buffs/Combo.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/buffs/Combo.java
@@ -35,7 +35,6 @@ import com.shatteredpixel.shatteredpixeldungeon.mechanics.Ballistica;
 import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.scenes.CellSelector;
 import com.shatteredpixel.shatteredpixeldungeon.scenes.GameScene;
-import com.shatteredpixel.shatteredpixeldungeon.sprites.CharSprite;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSprite;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
 import com.shatteredpixel.shatteredpixeldungeon.ui.ActionIndicator;
@@ -49,7 +48,6 @@ import com.watabou.noosa.audio.Sample;
 import com.watabou.utils.Bundle;
 import com.watabou.utils.Callback;
 import com.watabou.utils.PathFinder;
-import com.watabou.utils.Random;
 
 public class Combo extends Buff implements ActionIndicator.Action {
 	
@@ -250,7 +248,7 @@ public class Combo extends Buff implements ActionIndicator.Action {
 	public static class ParryTracker extends FlavourBuff{
 		{ actPriority = HERO_PRIO+1;}
 
-		public boolean parried;
+		public boolean  parried;
 
 		@Override
 		public void detach() {
