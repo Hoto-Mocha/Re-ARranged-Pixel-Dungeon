9c8615c463c6ca2a7e150984ba351583581938d4
Hoto-Mocha <mtj2010@naver.com>
2024-09-07 02:29:33 +0900

    회복의 알약에 의약공포증 효과 추가

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/pills/PillOfHealing.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/pills/PillOfHealing.java
@@ -1,9 +1,14 @@
 package com.shatteredpixel.shatteredpixeldungeon.items.pills;
 
+import com.shatteredpixel.shatteredpixeldungeon.Challenges;
+import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
 import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Healing;
+import com.shatteredpixel.shatteredpixeldungeon.items.potions.PotionOfHealing;
+import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
+import com.shatteredpixel.shatteredpixeldungeon.utils.GLog;
 
 public class PillOfHealing extends Pill {
     {
@@ -13,6 +18,10 @@ public class PillOfHealing extends Pill {
     @Override
     public void useEffect(Char appliedCh) {
         super.useEffect(appliedCh);
-        Buff.affect(appliedCh, Healing.class).setHeal((int)(appliedCh.HT*0.2f), 0, 1);
+        if (appliedCh == Dungeon.hero && Dungeon.isChallenged(Challenges.NO_HEALING)){
+            PotionOfHealing.pharmacophobiaProc(Dungeon.hero);
+        } else {
+            Buff.affect(appliedCh, Healing.class).setHeal((int)(appliedCh.HT*0.2f), 0, 1);
+        }
     }
 }
