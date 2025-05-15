3c0b0ac9513a6e9c70f5b31737cfd125784bb73c
Hoto-Mocha <mtj2010@naver.com>
2025-05-15 17:57:45 +0900

    건물이 광란에 면역이 되도록 수정

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/npcs/building/Building.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/npcs/building/Building.java
@@ -1,7 +1,10 @@
 package com.shatteredpixel.shatteredpixeldungeon.actors.mobs.npcs.building;
 
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Amok;
 import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.npcs.NPC;
 
 public class Building extends NPC {
-
+    {
+        immunities.add(Amok.class);
+    }
 }
