94e29fa9f4aa8c2be6b50976ae2f327d259ab836
Hoto-Mocha <mtj2010@naver.com>
2025-06-22 01:47:53 +0900

    건물이 공포에 면역이 되도록 수정

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/npcs/building/Building.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/npcs/building/Building.java
@@ -1,10 +1,12 @@
 package com.shatteredpixel.shatteredpixeldungeon.actors.mobs.npcs.building;
 
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Amok;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Terror;
 import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.npcs.NPC;
 
 public class Building extends NPC {
     {
         immunities.add(Amok.class);
+        immunities.add(Terror.class);
     }
 }
