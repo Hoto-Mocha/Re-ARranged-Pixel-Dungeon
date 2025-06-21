3328381f641d653f0af606a2132513bf0a5d1273
Hoto-Mocha <mtj2010@naver.com>
2025-06-22 03:34:31 +0900

    야포: 방어력 증가

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/npcs/building/Cannon.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/npcs/building/Cannon.java
@@ -45,24 +45,8 @@ public class Cannon extends Building {
 
     @Override
     public int drRoll() {
-        return super.drRoll() + Random.NormalIntRange(0, 5);
+        return super.drRoll() + Random.NormalIntRange(10, 30);
     }
-
-//    @Override
-//    protected boolean act() {
-//        for (Char ch : Actor.chars()) {
-//            if (ch instanceof Mob) {
-//                Mob enemy = (Mob)ch;
-//                if (enemy.targetingChar() == this
-//                        && enemy.fieldOfView != null
-//                        && enemy.fieldOfView[Dungeon.hero.pos]) {
-//                    enemy.aggro(Dungeon.hero);
-//                }
-//            }
-//        }
-//
-//        return super.act();
-//    }
 
     @Override
     public void destroy() {
