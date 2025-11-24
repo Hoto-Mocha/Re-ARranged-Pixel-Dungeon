ef3503e37c7487808bc62c47a99f9d3485706447
Mocha <mtj2010@naver.com>
2025-11-24 20:12:16 +0900

    사무라이: 재빠른 납도 특성이 다른 직업에 적용되지 않는 문제 수정

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/Hero.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/Hero.java
@@ -3312,6 +3312,9 @@ public class Hero extends Char {
 				Buff.affect(this, Sheath.Sheathing.class);
 			}
 		}
+		if (heroClass != HeroClass.SAMURAI && hasTalent(Talent.QUICK_SHEATHING) && !attackTarget.isAlive()) {
+			Buff.affect(this, Haste.class, 3f*pointsInTalent(Talent.QUICK_SHEATHING));
+		}
 		curAction = null;
 		attackTarget = null;
 
